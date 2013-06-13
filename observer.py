c39455faed25ccb44f258df02ae1ce2a7b699572
lcyang <lcyanggm@gmail.com>
2013-06-13 20:05:54 +0800

    修改了观察者模式

--- observer.py
+++ observer.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
 class AbstractSubject(object):
     def register(self, listener):
-        raise NotImplementedError("Must subclass me")
-
-    def unregister(self, listener):
-        raise NotImplementedError("Must subclass me")
-
-    def notify_listeners(self, event):
-        raise NotImplementedError("Must subclass me")
-
-class Subject(AbstractSubject):
-    def __init__(self):
-        self.listeners = []
-
-    def register(self, listener):
         self.listeners.append(listener)
 
     def unregister(self, listener):
         self.listeners.remove(listener)
+
+    def notify_listeners(self, event):
+        raise NotImplementedError("Must subclass me")
+
+
+class AbstractObserver(object):
+    def __init__(self, name, subject):
+        self.name = name
+        subject.register(self)
+
+    def notice(self):
+        raise NotImplementedError("Must subclass me!")
+
+
+class Subject(AbstractSubject):
+    def __init__(self):
+        self.listeners = []
 
     def notify_listeners(self, event):
         for listener in self.listeners:
@@ -27,21 +31,23 @@ class Subject(AbstractSubject):
         event = raw_input("input your event:")
         return event
 
-class Listener(object):
-    def __init__(self, name, subject):
-        self.name = name
-        subject.register(self)
 
+class ListenerA(AbstractObserver):
     def notice(self, event):
-        print self.name + " received " + event
+        print "ListenerA: " + self.name + " received " + event
+
+
+class ListenerB(AbstractObserver):
+    def notice(self, event):
+        print "ListernB: " + self.name + " received "+ event
 
 
 if __name__ == '__main__':
     subject = Subject()
 
-    listenerA = Listener("lcy", subject)
-    listenerB = Listener("lxx", subject)
+    listenerA = ListenerA("lcy", subject)
+    listenerB = ListenerB("lxx", subject)
     subject.notify_listeners("news")
 
-    action = subject.getUserAction()
-    subject.notify_listeners(action)
+    # action = subject.getUserAction()
+    # subject.notify_listeners(action)
